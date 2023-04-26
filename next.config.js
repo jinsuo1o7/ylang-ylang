219044b534f0d317e6b98ef2a5a4e4c2365cf06e
Jinsuo <cil04110@gmail.com>
2023-04-27 01:41:26 +0900

    feat: add supabase client supabase 클라이언트를 가져오기 위해 환경변수 세팅 nextconfig에서 storage 이미지를 url 경로 설정

--- next.config.js
+++ next.config.js
@@ -3,6 +3,14 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
-}
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "dicdyetebblmqjwiespn.supabase.co",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
